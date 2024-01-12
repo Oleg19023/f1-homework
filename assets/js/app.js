85f5b7b003b73ce0216f50244fbc91889b04547c
Oleg19023 <96693781+Oleg19023@users.noreply.github.com>
2024-01-12 23:39:09 +0200

    bags....

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,90 @@
-function checkTINFromForm() {
-    const tin = document.getElementById('tinNumber').value;
-    const result = checkTIN(tin);
-    document.getElementById('resultContainer').innerText = result;
+// Загрузка списка стран
+fetch('https://restcountries.com/v3.1/all')
+    .then(response => response.json())
+    .then(countries => {
+        // Загрузка курсов валют НБУ
+        fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+            .then(response => response.json())
+            .then(currencies => {
+                // Обработка данных и отображение результатов
+                displayResults(countries, currencies);
+                
+        // Добавление текущей даты в заголовок
+        const date = currencies[0].exchangedate; // Получение даты из первого объекта в массиве валют
+        const titleElement = document.querySelector('h1');
+        titleElement.textContent += ` на ${date}`; // Добавление даты к тексту заголовка
+
+            });
+    });
+
+// function displayResults(countries, currencies) {
+//     const container = document.getElementById('currencyContainer');
+//     const orderedCountries = ['Australia', 'Canada', 'China', 'Czech Republic', 'Denmark', 'Hong Kong', 'Hungary', 'India', 'Indonesia', 'Israel', 'Japan', 'Kazakhstan', 'South Korea', 'Mexico', 'Moldova', 'New Zealand', 'Norway', 'Russia', 'Singapore', 'South Africa', 'Sweden', 'Switzerland', 'Egypt', 'United Kingdom', 'United States', 'Belarus', 'Azerbaijan', 'Romania', 'Turkey', 'SDR', 'Bulgaria', 'Euro', 'Poland', 'Algeria', 'Bangladesh', 'Armenia', 'Dominican Republic', 'Iran', 'Iraq', 'Kyrgyzstan', 'Lebanon', 'Libya', 'Malaysia', 'Morocco', 'Pakistan', 'Saudi Arabia', 'Vietnam', 'Thailand', 'UAE', 'Tunisia', 'Uzbekistan', 'Taiwan', 'Turkmenistan', 'Serbia', 'Tajikistan', 'Georgia', 'Brazil', 'Gold', 'Silver', 'Platinum', 'Palladium'];
+
+//     orderedCountries.forEach(countryName => {
+//     const country = countries.find(c => c.name.common === countryName);
+//     if (country && country.flags) {
+//         const flagElement = document.createElement('img');
+//         flagElement.src = country.flags.png;
+//         flagElement.title = country.name.common; // This will add a tooltip with the country name
+//         flagElement.style.height = '60px';
+//         flagElement.style.width = '90px';
+//         container.appendChild(flagElement);
+//         container.appendChild(document.createElement('br')); // This will add a line break after each flag
+//     }
+
+//     currencies.forEach(currency => {
+//         // Создать элемент для отображения информации о валюте
+//         const currencyElement = document.createElement('div');
+//         currencyElement.innerHTML = `
+//                 <h2>${currency.txt} (${currency.cc})</h2>
+//                 <p>Курс: ${currency.rate} грн</p>
+//             `;
+//         container.appendChild(currencyElement);
+//     });
+//     });
+// }
+
+function displayResults(countries, currencies) {
+    const container = document.getElementById('currencyContainer');
+    const orderedCountries = ['Australia', 'Canada', 'China', 'Czech Republic', 'Denmark', 'Hong Kong', 'Hungary', 'India', 'Indonesia', 'Israel', 'Japan', 'Kazakhstan', 'South Korea', 'Mexico', 'Moldova', 'New Zealand', 'Norway', 'Russia', 'Singapore', 'South Africa', 'Sweden', 'Switzerland', 'Egypt', 'United Kingdom', 'United States', 'Belarus', 'Azerbaijan', 'Romania', 'Turkey', 'SDR', 'Bulgaria', 'Euro', 'Poland', 'Algeria', 'Bangladesh', 'Armenia', 'Dominican Republic', 'Iran', 'Iraq', 'Kyrgyzstan', 'Lebanon', 'Libya', 'Malaysia', 'Morocco', 'Pakistan', 'Saudi Arabia', 'Vietnam', 'Thailand', 'UAE', 'Tunisia', 'Uzbekistan', 'Taiwan', 'Turkmenistan', 'Serbia', 'Tajikistan', 'Georgia', 'Brazil', 'Gold', 'Silver', 'Platinum', 'Palladium'];
+
+    orderedCountries.forEach(countryName => {
+        const country = countries.find(c => c.name.common === countryName);
+        if (country && country.flags) {
+            const flagElement = document.createElement('img');
+            flagElement.src = country.flags.png;
+            flagElement.title = country.name.common; // This will add a tooltip with the country name
+            flagElement.style.height = '60px';
+            flagElement.style.width = '90px';
+            container.appendChild(flagElement);
+            container.appendChild(document.createElement('br')); // This will add a line break after each flag
+
+            const currency = currencies.find(c => {
+                if (country.currencies) {
+                    return Object.values(country.currencies).some(c => c.code === c.c);
+                }
+            });
+
+            if (currency) {
+                const currencyElement = document.createElement('div');
+                currencyElement.innerHTML = `
+                    <h2>${currency.txt} (${currency.cc})</h2>
+                    <p>Курс: ${currency.rate} грн</p>
+                `;
+                container.appendChild(currencyElement);
+            }
+        }
+    });
 }
 
-function checkTIN(tin) {
-    // ИНН формат
-    if (!/^\d{10}$/.test(tin)) {
-        return "Некорректный ИНН";
-    }
-
-    // Дата рожд
-    const birthDays = parseInt(tin.substring(0, 5));
-    const birthDate = new Date(1899, 11, 31);
-    birthDate.setDate(birthDate.getDate() + birthDays);
-
-    // Пол
-    const gender = tin.substring(8, 9) % 2 == 0 ? "Женский" : "Мужской";
-
-    // Возраст
-    const today = new Date();
-    let age = today.getFullYear() - birthDate.getFullYear();
-    const m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-
-    return "ИНН корректен. Дата рождения: " + birthDate.toLocaleDateString() + ". Пол: " + gender + ". Полных лет: " + age;
-}
+// Загрузка списка стран
+fetch('https://restcountries.com/v3.1/all')
+    .then(response => response.json())
+    .then(countries => {
+        countries.forEach(country => {
+            if (country.flags) {
+                console.log(country.flags.png); // Вывод ссылки на флаг страны
+            }
+        });
+    });
