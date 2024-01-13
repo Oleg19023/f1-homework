7e5c2e3d952b92269bb961449ea5e7cf824e3f44
Oleg19023 <96693781+Oleg19023@users.noreply.github.com>
2024-01-13 22:32:19 +0200

    commit-5

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,82 +1,57 @@
-// Загрузка списка стран
-fetch('https://restcountries.com/v3.1/all')
+// Загрузка курса валюты
+fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
     .then(response => response.json())
-    .then(countries => {
-        // Загрузка курсов валют
-        fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
-            .then(response => response.json())
-            .then(currencies => {
-                // Обработка
-                displayResults(countries, currencies);        
+    .then(currencies => {
+        // Вывод даты курса
         const date = currencies[0].exchangedate;
         const titleElement = document.querySelector('h1');
         titleElement.textContent += ` на ${date}`;
-            });
+
+        // На каждую валюту - элемент
+        currencies.forEach(currency => {
+            const currencyElement = document.createElement('div');
+            currencyElement.style.display = 'flex';
+            currencyElement.style.flexWrap = 'wrap';
+            currencyElement.style.alignItems = 'center';
+            currencyElement.style.border = '1px solid black';
+            currencyElement.style.backgroundColor = 'rgba(235, 235, 235)';
+            currencyElement.style.padding = '10px';
+            currencyElement.style.margin = '10px auto';
+            currencyElement.style.maxWidth = '1500px';
+
+            const infoElement = document.createElement('div');
+            infoElement.innerHTML = `
+                <h2>${currency.txt} (${currency.cc})</h2>
+                <p>Курс: ${currency.rate} грн</p>
+            `;
+            infoElement.style.marginRight = '10px';
+            currencyElement.appendChild(infoElement);
+
+            // Загрузка флагов
+            fetch(`https://restcountries.com/v3.1/currency/${currency.cc}`)
+                .then(response => response.json())
+                .then(countries => {
+                    countries.forEach((country, index) => {
+                        const flagElement = document.createElement('img');
+                        flagElement.src = country.flags.png;
+                        flagElement.title = country.name.common;
+                        flagElement.style.height = '60px';
+                        flagElement.style.width = '90px';
+                        flagElement.style.marginRight = '10px';
+                        flagElement.style.marginBottom = '10px';
+
+                        // Перенос
+                        if (index % 5 === 0) {
+                            flagElement.style.flexBasis = 0;
+                        }
+                        currencyElement.appendChild(flagElement);
+                    });
+                });
+
+            document.body.appendChild(currencyElement);
+            console.log(currency);
+        });
     });
-
-// function displayResults(countries, currencies) {
-//     const container = document.getElementById('currencyContainer');
-//     const orderedCountries = ['Australia', 'Canada', 'China', 'Czech Republic', 'Denmark', 'Hong Kong', 'Hungary', 'India', 'Indonesia', 'Israel', 'Japan', 'Kazakhstan', 'South Korea', 'Mexico', 'Moldova', 'New Zealand', 'Norway', 'Russia', 'Singapore', 'South Africa', 'Sweden', 'Switzerland', 'Egypt', 'United Kingdom', 'United States', 'Belarus', 'Azerbaijan', 'Romania', 'Turkey', 'SDR', 'Bulgaria', 'Euro', 'Poland', 'Algeria', 'Bangladesh', 'Armenia', 'Dominican Republic', 'Iran', 'Iraq', 'Kyrgyzstan', 'Lebanon', 'Libya', 'Malaysia', 'Morocco', 'Pakistan', 'Saudi Arabia', 'Vietnam', 'Thailand', 'UAE', 'Tunisia', 'Uzbekistan', 'Taiwan', 'Turkmenistan', 'Serbia', 'Tajikistan', 'Georgia', 'Brazil', 'Gold', 'Silver', 'Platinum', 'Palladium'];
-
-//     orderedCountries.forEach(countryName => {
-//     const country = countries.find(c => c.name.common === countryName);
-//     if (country && country.flags) {
-//         const flagElement = document.createElement('img');
-//         flagElement.src = country.flags.png;
-//         flagElement.title = country.name.common; // This will add a tooltip with the country name
-//         flagElement.style.height = '60px';
-//         flagElement.style.width = '90px';
-//         container.appendChild(flagElement);
-//         container.appendChild(document.createElement('br')); // This will add a line break after each flag
-//     }
-
-//     currencies.forEach(currency => {
-//         // отображения информации о валюте
-//         const currencyElement = document.createElement('div');
-//         currencyElement.innerHTML = `
-//                 <h2>${currency.txt} (${currency.cc})</h2>
-//                 <p>Курс: ${currency.rate} грн</p>
-//             `;
-//         container.appendChild(currencyElement);
-//     });
-//     });
-// }
-
-function displayResults(countries, currencies) {
-    const container = document.getElementById('currencyContainer');
-    const orderedCountries = ['Australia', 'Canada', 'China', 'Czech Republic', 'Denmark', 'Hong Kong', 'Hungary', 'India', 'Indonesia', 'Israel', 'Japan', 'Kazakhstan', 'South Korea', 'Mexico', 'Moldova', 'New Zealand', 'Norway', 'Russia', 'Singapore', 'South Africa', 'Sweden', 'Switzerland', 'Egypt', 'United Kingdom', 'United States', 'Belarus', 'Azerbaijan', 'Romania', 'Turkey', 'SDR', 'Bulgaria', 'Euro', 'Poland', 'Algeria', 'Bangladesh', 'Armenia', 'Dominican Republic', 'Iran', 'Iraq', 'Kyrgyzstan', 'Lebanon', 'Libya', 'Malaysia', 'Morocco', 'Pakistan', 'Saudi Arabia', 'Vietnam', 'Thailand', 'UAE', 'Tunisia', 'Uzbekistan', 'Taiwan', 'Turkmenistan', 'Serbia', 'Tajikistan', 'Georgia', 'Brazil', 'Gold', 'Silver', 'Platinum', 'Palladium'];
-
-    orderedCountries.forEach(countryName => {
-        const country = countries.find(c => c.name.common === countryName);
-        if (country && country.flags) {
-            const flagElement = document.createElement('img');
-            flagElement.src = country.flags.png;
-            flagElement.title = country.name.common;
-            flagElement.style.height = '60px';
-            flagElement.style.width = '90px';
-            container.appendChild(flagElement);
-            container.appendChild(document.createElement('br'));
-
-            const currency = currencies.find(c => {
-                if (country.currencies) {
-                    return Object.values(country.currencies).some(c => c.code === c.c);
-                }
-            });
-
-            if (currency) {
-                const currencyElement = document.createElement('div');
-                currencyElement.innerHTML = `
-                    <h2>${currency.txt} (${currency.cc})</h2>
-                    <p>Курс: ${currency.rate} грн</p>
-                `;
-                container.appendChild(currencyElement);
-            }
-        }
-    });
-}
-
-
-
 
 fetch('https://restcountries.com/v3.1/all')
     .then(response => response.json())
